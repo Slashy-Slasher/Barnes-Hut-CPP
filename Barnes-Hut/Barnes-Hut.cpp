ee17334e6c7570df63c792f1369ae27932b09d37
Slashy Slasher <aardvarkchase@gmail.com>
2025-07-30 22:19:41 -0400

    Add project files.

---
+++ Barnes-Hut/Barnes-Hut.cpp
@@ -0,0 +1,12 @@
+﻿// Barnes-Hut.cpp : Defines the entry point for the application.
+//
+
+#include "Barnes-Hut.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
