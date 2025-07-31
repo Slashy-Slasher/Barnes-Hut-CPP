48874616ff81ce9616e120578c820bd40cb2237f
Slashy Slasher <aardvarkchase@gmail.com>
2025-07-30 23:27:16 -0400

    Stated Scoping out the Quadtree - Added Variables - Stated mapping out methods in notes - Was held up by cmake's usage so I had to remove raylib for now

--- Barnes-Hut/Barnes-Hut.cpp
+++ Barnes-Hut/Barnes-Hut.cpp
@@ -2,11 +2,47 @@
 //
 
 #include "Barnes-Hut.h"
+#include <raylib>
+#include <iomanip>
+#include <cmath>
+#include <random>
 
 using namespace std;
+
+class Quadtree
+{
+	float x;			//Top x_left corner of the area
+	float y;			//Top y_left corner of the area
+	float w;			//Bottom x_right corner of the area
+	float h;			//Bottom y_right corner of the area
+	float width;		//Width of the box
+
+	int max;			//Max depth
+	int depth;			//Current Depth
+
+	Quadtree* parent;;  //Points backwards to the parent
+	Quadtree* tlc;		//Pointer to the top left corner
+	Quadtree* trc;		//Pointer to the top right corner
+	Quadtree* blc;		//Pointer to the bottom left corner
+	Quadtree* brc;		//Pointer to the bottom right corner
+	bool isRoot;
+
+	Quadtree()
+	{
+
+	}
+	subdivide()
+	{
+
+	}
+
+
+};
 
 int main()
 {
 	cout << "Hello CMake." << endl;
 	return 0;
 }
+
+
