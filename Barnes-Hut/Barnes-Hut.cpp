6f7e5ae57ae15b73ef8c01c36444515e37184210
Slashy Slasher <aardvarkchase@gmail.com>
2025-07-31 23:57:47 -0400

    Changes and Comments
    -Planned out more of how this program will differ from the python version, right from the bat I'm going to be creating the quadtree specifically for this project
    -Expanded quadtree code
    -Created Planet skeleton
    -Spent more time learning about cmake lists, so I'm hoping that future updates will be more impactful

--- Barnes-Hut/Barnes-Hut.cpp
+++ Barnes-Hut/Barnes-Hut.cpp
@@ -9,6 +9,12 @@
 
 using namespace std;
 
+
+class Planet
+{
+
+};
+
 class Quadtree
 {
 	float x;			//Top x_left corner of the area
@@ -20,22 +26,50 @@ class Quadtree
 	int max;			//Max depth
 	int depth;			//Current Depth
 
-	Quadtree* parent;;  //Points backwards to the parent
+	Quadtree* parent;  //Points backwards to the parent
 	Quadtree* tlc;		//Pointer to the top left corner
 	Quadtree* trc;		//Pointer to the top right corner
 	Quadtree* blc;		//Pointer to the bottom left corner
 	Quadtree* brc;		//Pointer to the bottom right corner
+
 	bool isRoot;
 
-	Quadtree()
+	vector<Planet> region_planets;	//All planets in the region
+	float combined_planet_mass;
+
+
+
+	float calculate_region_mass()
+	{
+		return 0; //Returns the entire region's mass
+	}
+
+	void check_root()
+	{
+		if (parent == is_null_pointer)
+		{
+			isRoot = true;
+		}
+	}
+
+	void subdivide()
 	{
 
 	}
-	subdivide()
+
+	Quadtree(Quadtree* parent, float x, float y, float w, float h, vector<Planet> planet_list, int depth, int max)	//Generates a quadtree and returns a pointer
 	{
-
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		this->parent = parent;
+		this->region_planets = planet_list;
+		this->depth = depth;
+		this->max = max;
+		check_root();	//Determines if the Quadtree has a parent node and configures the bool accordingly
+		this->combined_planet_mass = calculate_region_mass();
 	}
-
 
 };
 
